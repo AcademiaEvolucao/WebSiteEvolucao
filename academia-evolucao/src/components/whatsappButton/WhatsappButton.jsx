78e6a66dc50cbb16fce7088b8cc607e663ef3590
Academia Evolução <217347442+AcademiaEvolucao@users.noreply.github.com>
2025-06-23 08:28:07 -0300

    Updates

--- academia-evolucao/src/components/whatsappButton/WhatsappButton.jsx
+++
@@ -1,7 +0,0 @@
-import { WhatsAppOutlined } from "@ant-design/icons";
-import './WhatsAppButton.css'
-export default function WhatsAppButton ({children, darkTheme}){
-  return(
-    <a href="https://wa.me/message/JIYIDMVOUKSKJ1" className={darkTheme ? "whats-button-dark-themed" : "whats-button"}>{children}<WhatsAppOutlined style={{marginLeft: '10px'}}/></a>
-  );
-}
